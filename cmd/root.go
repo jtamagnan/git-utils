6f3d667a12b40e860ddde50a220fd3ed45588477
Jules Tamagnan <jtamagnan@gmail.com>
2025-04-19 09:56:41 -0700

    Start project

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "directorylist",
+	Short: "List the size of a local directory.",
+	Long:  `This command will display the size of a directory with several different options.`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
